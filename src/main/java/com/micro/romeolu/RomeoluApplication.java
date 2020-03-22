d7e6950257b75c01a29d340248369ae726f9fbc7
lup11 <lup11@chinaunicom.cn>
2020-03-22 20:27:59 +0800

    提交工程

---
+++ src/main/java/com/micro/romeolu/RomeoluApplication.java
@@ -0,0 +1,13 @@
+package com.micro.romeolu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomeoluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomeoluApplication.class, args);
+    }
+
+}
